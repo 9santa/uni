3a0c0de0c295d74db5f9709a167bd96fa8a22959
9santa <64308510+9santa@users.noreply.github.com>
2024-10-23 10:52:06 +0300

    Лабораторная работа #3 Рекурсия

---
+++ lab3.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+using ll         =     long long;
+
+#define F              first
+#define S              second
+#define pb             push_back
+#define all(x)         x.begin(), x.end()
+#define debug(x) cout << __FUNCTION__ << ":" << __LINE__ << " " << #x << " = " << x << endl
+ 
+const int N = 1e5;
+const int mod = 998244353;
+
+
+vector<int> graph[N];
+vector<bool> vis(N);
+vector<bool> col(N);
+
+bool flag = 0;
+
+int razlom(int n,int m)
+	{
+		if (n == 1 && m == 1){
+			return 0;
+		}
+		if (n == 1){
+			return m-1;
+		}
+		if (m == 1){
+			return n-1;
+		}
+		int ans = razlom((n+2-1)/2,m) + razlom(n-(n+2-1)/2,m) + 1;
+		return ans;
+	}
+
+// void cohle()        
+// {
+// 	int n,m; cin >> n >> m;
+
+
+// }
+ 
+ 
+int main()
+{
+	// freopen("ancestor.in","rt",stdin);
+	// freopen("ancestor.out","wt",stdout);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	clock_t z = clock();
+	
+	int n,m; cin >> n >> m;
+	cout << razlom(n,m);
+	// cohle();
+
+	
+	// int t; cin >> t;
+	// while(t--) cohle();
+	
+	cerr << "\nRun Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+	
+	return 0;
+}
